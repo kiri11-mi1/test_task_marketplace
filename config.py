38d3348126c3f06cf1800c7dc5673f10f02ac557
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-04-25 15:41:22 +0700

    файл настройки

---
+++ config.py
@@ -0,0 +1,17 @@
+FIELDS = [
+    'Дата',
+    'Ссылка на изображение',
+    'Ссылка на товар',
+    'Артикул',
+    'Прайс',
+    'Рейтинг',
+    'Остаток',
+    'Комментариев за день',
+]
+
+URLS = [
+    'https://www.wildberries.ru/catalog/15545307/detail.aspx?targetUrl=SP',
+    'https://www.wildberries.ru/catalog/19957874/detail.aspx?targetUrl=XS',
+    'https://www.wildberries.ru/catalog/19247096/detail.aspx?targetUrl=XS',
+    'https://www.wildberries.ru/catalog/10017305/detail.aspx?targetUrl=XS'
+]
